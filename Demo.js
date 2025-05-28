0626f93fc537af8ee6d115105347cb1ff1225e11
PhucTran-2002 <138229267+PhucTran-2002@users.noreply.github.com>
2025-05-28 15:58:52 +0700

    Add files via upload

---
+++ Demo.js
@@ -0,0 +1,30 @@
+// Khai báo biến
+let age = 233; 
+const PI = 3.14; 
+var oldVariable = "deprecated"; // không nên dùng (cũ)
+
+// Các kiểu dữ liệu cơ bản
+const name = "Phuc TranTran"; // string
+const score = 100; // number
+const isActive = true; // boolean
+const person = { firstName: "Phuc", lastName: "TranTran" }; // object
+const numbers = [1, 2, 3]; // array
+let emptyValue = null;  //null: Giá trị rỗng (được gán chủ động)
+let x; // undefined  Chưa được gán giá trị
+let id = Symbol("unique_id"); //symbol (ES6): Giá trị duy nhất, dùng làm key cho object
+
+
+// Toán tử số học
+let sum = 10 + 5; 
+let remainder = 10 % 3; //chia lấy dư
+
+// Toán tử so sánh
+console.log(10 > 5); // true
+console.log(10 === "10"); // false (so sánh cả kiểu dữ liệu)
+
+// Toán tử logic
+console.log(true && false); // false (AND)
+console.log(true || false); // true (OR)
+
+
+
---
+++ Loops.js
@@ -0,0 +1,17 @@
+// for loop
+for (let i = 0; i < 5; i++) {
+    console.log(i); 
+  }
+  
+  // while loop
+  let count = 0;
+  while (count < 10) {
+    console.log(count);
+    count++;
+  }
+  
+  // for...of (dùng với array)
+  const fruits = ["apple", "banana", "orange"];
+  for (const fruit of fruits) {
+    console.log(fruit);
+  }
