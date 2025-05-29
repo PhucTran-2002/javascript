66e81461995755214cc13334da6127f6da65f1ff
PhucTran-2002 <138229267+PhucTran-2002@users.noreply.github.com>
2025-05-29 16:45:10 +0700

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,47 @@
+
+//Arrow Function  ham mui ten
+const add = (a, b) => {
+    return a + b;
+  };
+  
+  console.log(add(3, 4));
+
+  //
+const multiply = (a, b) => a * b;
+console.log(multiply(2, 3));
+
+
+//Function Declaration ham khai bao
+
+
+function hiName(name) {
+    console.log("Hello, " + name);
+  }
+  
+  hiName("Phuc");
+
+  //Function Expression  ham bieu thuc
+
+  const sayGoodbye = function(name) {
+    console.log("Goodbye, " + name);
+  };
+  
+  sayGoodbye("Phuc");
+
+  //dung lam Callback Function
+
+const numbers = [1, 2, 3];
+
+numbers.forEach(function(num) {
+  console.log(num * 2);
+});
+
+
+
+// khoi tao doi tuong
+function Person(name) {
+    this.name = name;
+  }
+  
+const p1 = new Person("Bob");
+console.log(p1.name);
